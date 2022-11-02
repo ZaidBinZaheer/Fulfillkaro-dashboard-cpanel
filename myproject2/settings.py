d2488608fc932243c217c79a5b9a772f4e1a9abb
Zaid <zaidbinzaheer20@gmail.com>
2022-11-02 13:54:25 +0500

    commit 5

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'fulfillkaro',
+        'HOST':'127.0.0.1',
+        'PORT':'3307',
+        'USER':'root',
+        'PASSWORD':'25october1999'
     }
 }
 
